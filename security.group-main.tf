5b55eb259b9e0be5d6ae06c9f7326239c05455b3
SandeepRoshan <115892000+SandeepRoshan@users.noreply.github.com>
2024-08-19 14:41:39 +0530

    add multiple resources

--- security.group-main.tf
+++ security.group-main.tf
@@ -1,72 +1,40 @@
-
 locals {
-
     security_group_tags = {
-        Name = "security-group-${ var.in_ecosystem }-${ var.in_timestamp }"
-        Desc = "New security group for ${ var.in_ecosystem } ${ var.in_description }"
+        Name = "security-group-${ var.in_ecosystem }-${ var.in_timestamp }-${ count.index }"
+        Desc = "New security group ${ var.in_ecosystem } ${ var.in_description } ${ count.index }"
     }
-
 }
 
-/*
- | --
- | -- This is the main security group resource for aggregating the
- | -- ingress and egress rules. It is important for now to always
- | -- create another security group as Terraform cannot easily handle
- | -- importing and changing the VPC's default security group.
-*/
 resource "aws_security_group" "new" {
-
     count       = var.in_sg_count
     vpc_id      = var.in_vpc_id
-    name        = "security-group-${ var.in_ecosystem }-${ var.in_timestamp }-n"
-    description = "This new security group ${ var.in_description }"
-    tags        = merge( local.security_group_tags, var.in_mandated_tags )
-
+    name        = "security-group-${ var.in_ecosystem }-${ var.in_timestamp }-${ count.index }"
+    description = "This new security group ${ var.in_description } ${ count.index }"
+    tags        = merge(local.security_group_tags, var.in_mandated_tags)
 }
 
-
-/*
- | --
- | -- Add the incoming ingress rules to the aggregating security
- | -- group. The cidr blocks define the source from which traffic
- | -- can flow. You can pass in 0.0.0.0/0 for anywhere but also
- | -- specify IP address ranges right down to a single host.
-*/
-resource aws_security_group_rule ingress {
-
-    count = length( var.in_ingress )
-    security_group_id = aws_security_group.new.id
+resource "aws_security_group_rule" "ingress" {
+    count = length(var.in_ingress)
+    security_group_id = aws_security_group.new[count.index % var.in_sg_count].id
 
     type        = "ingress"
     cidr_blocks = var.in_ingress_cidr_blocks
-    description = element( var.rules[ var.in_ingress[ count.index ] ], 3 )
+    description = element(var.rules[var.in_ingress[count.index]], 3)
 
-    from_port = element( var.rules[ var.in_ingress[ count.index ] ], 0 )
-    to_port   = element( var.rules[ var.in_ingress[ count.index ] ], 1 )
-    protocol  = element( var.rules[ var.in_ingress[ count.index ] ], 2 )
+    from_port = element(var.rules[var.in_ingress[count.index]], 0)
+    to_port   = element(var.rules[var.in_ingress[count.index]], 1)
+    protocol  = element(var.rules[var.in_ingress[count.index]], 2)
 }
 
-
-/*
- | --
- | -- Add the outgoing egress rules to the aggregating security
- | -- group. The cidr blocks define the destinations to which traffic
- | -- can flow. You can pass in 0.0.0.0/0 for anywhere or you can
- | -- specify IP address ranges right down to a single destined host.
-*/
-resource aws_security_group_rule egress {
-
-    count = length( var.in_egress )
-    security_group_id = aws_security_group.new.id
+resource "aws_security_group_rule" "egress" {
+    count = length(var.in_egress)
+    security_group_id = aws_security_group.new[count.index % var.in_sg_count].id
 
     type        = "egress"
     cidr_blocks = var.in_egress_cidr_blocks
-    description = element( var.rules[ var.in_egress[ count.index ] ], 3 )
+    description = element(var.rules[var.in_egress[count.index]], 3)
 
-    from_port = element( var.rules[ var.in_egress[ count.index ] ], 0 )
-    to_port   = element( var.rules[ var.in_egress[ count.index ] ], 1 )
-    protocol  = element( var.rules[ var.in_egress[ count.index ] ], 2 )
+    from_port = element(var.rules[var.in_egress[count.index]], 0)
+    to_port   = element(var.rules[var.in_egress[count.index]], 1)
+    protocol  = element(var.rules[var.in_egress[count.index]], 2)
 }
-
-
