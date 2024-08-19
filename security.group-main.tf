20c9eb1e9cdc64f9feaed19dd68f1af52b7ed0b5
SandeepRoshan <115892000+SandeepRoshan@users.noreply.github.com>
2024-08-19 14:53:28 +0530

    checkingg

--- security.group-main.tf
+++ security.group-main.tf
@@ -6,35 +6,43 @@ locals {
 }
 
 resource "aws_security_group" "new" {
-    count       = var.in_sg_count
-    vpc_id      = var.in_vpc_id
-    name        = "security-group-${ var.in_ecosystem }-${ var.in_timestamp }-${ count.index }"
-    description = "This new security group ${ var.in_description } ${ count.index }"
-    tags        = merge(local.security_group_tags, var.in_mandated_tags)
+  count       = var.in_sg_count
+  vpc_id      = var.in_vpc_id
+  name        = "security-group-${ var.in_ecosystem }-${ var.in_timestamp }-${ count.index }"
+  description = "This new security group ${ var.in_description } ${ count.index }"
+  tags        = merge(
+    {
+      Name = "security-group-${ var.in_ecosystem }-${ var.in_timestamp }-${ count.index }"
+      Desc = "New security group ${ var.in_ecosystem } ${ var.in_description } ${ count.index }"
+    },
+    var.in_mandated_tags
+  )
 }
 
+
 resource "aws_security_group_rule" "ingress" {
-    count = length(var.in_ingress)
-    security_group_id = aws_security_group.new[count.index % var.in_sg_count].id
+  count = length(var.in_ingress)
+  security_group_id = aws_security_group.new[count.index % var.in_sg_count].id
 
-    type        = "ingress"
-    cidr_blocks = var.in_ingress_cidr_blocks
-    description = element(var.rules[var.in_ingress[count.index]], 3)
+  type        = "ingress"
+  cidr_blocks = var.in_ingress_cidr_blocks
+  description = element(var.rules[var.in_ingress[count.index]], 3)
 
-    from_port = element(var.rules[var.in_ingress[count.index]], 0)
-    to_port   = element(var.rules[var.in_ingress[count.index]], 1)
-    protocol  = element(var.rules[var.in_ingress[count.index]], 2)
+  from_port = element(var.rules[var.in_ingress[count.index]], 0)
+  to_port   = element(var.rules[var.in_ingress[count.index]], 1)
+  protocol  = element(var.rules[var.in_ingress[count.index]], 2)
 }
 
 resource "aws_security_group_rule" "egress" {
-    count = length(var.in_egress)
-    security_group_id = aws_security_group.new[count.index % var.in_sg_count].id
+  count = length(var.in_egress)
+  security_group_id = aws_security_group.new[count.index % var.in_sg_count].id
 
-    type        = "egress"
-    cidr_blocks = var.in_egress_cidr_blocks
-    description = element(var.rules[var.in_egress[count.index]], 3)
+  type        = "egress"
+  cidr_blocks = var.in_egress_cidr_blocks
+  description = element(var.rules[var.in_egress[count.index]], 3)
 
-    from_port = element(var.rules[var.in_egress[count.index]], 0)
-    to_port   = element(var.rules[var.in_egress[count.index]], 1)
-    protocol  = element(var.rules[var.in_egress[count.index]], 2)
+  from_port = element(var.rules[var.in_egress[count.index]], 0)
+  to_port   = element(var.rules[var.in_egress[count.index]], 1)
+  protocol  = element(var.rules[var.in_egress[count.index]], 2)
 }
+
